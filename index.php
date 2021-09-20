cf03d3b676e4389f9849bf98620a55d327bc9359
Future OS <90888380+FutureOSLinux@users.noreply.github.com>
2021-09-20 05:15:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Future OS Package Repository</title>
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+<body>
+  <div class="link">
+    <a href="http://pkgs.futurelinux.org/future">Browse Directory -> /</a>
+  </div>  
+  <div id="future">
+    <img src="icons/future-logo-icon.svg" class="future-logo" />
+  </div>
+</body>
+</html>
